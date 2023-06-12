65e8dbebbd9be85ea81faba08d8abe9b8c43aeac
Kijun Kim <tlduf03@gmail.com>
2023-06-12 12:24:17 -0700

    The web app is ready

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ scaler = pickle.load(open('scaling.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict_api', methods=['POST']) #check with postman
 def predict_api():
     data = request.json['data']
     print(data)
@@ -24,6 +24,14 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0]) # Returned value is an numpy array
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    ouptut = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text='The House price prediction is {}'.format(ouptut))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
